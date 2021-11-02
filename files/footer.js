ee31cb845789bc7334939f1b559f0f6d196183ba
D++ <82842780+dplusplus1024@users.noreply.github.com>
2021-11-02 01:39:56 -0500

    Add files via upload

--- files/footer.js
+++ files/footer.js
@@ -2,9 +2,7 @@ document.write('\
 <footer>\
 <!--img src=images/logo-small.jpg><br-->\
 <i><a href=https://discord.gg/bKvdZS8DKa>Join the D++ <b>Discord</b> for future workshops!<br></a></i>\
-© 2021 D++  &nbsp;&nbsp;<a href=https://dplusplus.me>http://dplusplus.me</a>&nbsp;&nbsp;&nbsp;\
-<div id="hide-on-desktop"></div>\
-<a href=https://twitter.com/D_plus__plus><img src="images/Twitter.jpg" width=14> d_plus__plus</a>&nbsp;&nbsp;&nbsp;<a href=https://www.instagram.com/d_plus_plus/><img src="images/Instagram.jpg" width=14> d_plus_plus</a>\
+© 2021 D++  &nbsp;&nbsp;<a href=https://dplusplus.me>http://dplusplus.me</a><span id="hide-on-mobile">&nbsp;&nbsp;&nbsp;</span><div id="hide-on-desktop"></div><a href=https://twitter.com/D_plus__plus><img src="images/Twitter.jpg" width=14> d_plus__plus</a>&nbsp;&nbsp;&nbsp;<a href=https://www.instagram.com/d_plus_plus/><img src="images/Instagram.jpg" width=14> d_plus_plus</a>\
 </footer>\
 \
 ');
