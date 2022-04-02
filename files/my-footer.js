4937db0cc1a6b594eb78f7f426fb727c9e44802e
D++ <82842780+dplusplus1024@users.noreply.github.com>
2022-04-02 00:17:22 -0500

    Add files via upload

---
+++ files/my-footer.js
@@ -0,0 +1,6 @@
+document.write('\
+<!--img src=images/logo-small.jpg><br-->\
+<i style="font-size:110%"><a href=https://discord.gg/bKvdZS8DKa>Join the <b><u>D++ Discord</b></u>   for future workshops!<br></a></i>\
+© 2022 D++  <a href=https://dplusplus.me>http://dplusplus.me</a><span id="hide-on-mobile">&nbsp;&nbsp;&nbsp;</span><div id="hide-on-desktop"></div><a href=https://twitter.com/D_plus__plus><img src="images/Twitter.jpg" width=14> d_plus__plus</a>&nbsp;&nbsp;&nbsp;<a href=https://www.instagram.com/d_plus_plus/><img src="images/Instagram.jpg" width=14> d_plus_plus</a>\
+\
+');
